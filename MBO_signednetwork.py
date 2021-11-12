0adce852ef34253d113f7abda6ee171d9662a148
ZijunLee <65371650+ZijunLee@users.noreply.github.com>
2021-11-12 03:56:47 +0100

    Add files via upload

--- MBO_signednetwork.py
+++ MBO_signednetwork.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
-
+from sklearn.metrics.cluster import adjusted_rand_score
+from random import randrange
 
 """ Parameters
     -----------
@@ -18,65 +19,106 @@ import random
         stopping criterion for iteration
     laplacian_matrix_ : array-like, shape (n_samples, n_samples)
         signed Laplacian matrix
+    ground_truth : ndarray, (n_samples,).{0...K-1} labels.
+        labels corresponding to the raw data  
     """
 
-def mbo_modularity_eig(N,K,m,dt,laplacian_matrix_, tol = .5,inner_step_count = 5): # inner stepcount is actually important! and can't be set to 1...
+def mbo_modularity_eig(N,K,m,dt,laplacian_matrix_, tol ,inner_step_count): # inner stepcount is actually important! and can't be set to 1...
     
-    # Initialize parameters
-    EigVal, EigVec = np.linalg.eig(laplacian_matrix_)
-    Val_m = EigVal[m]
-    Vec_m = EigVec[m]
-    B_m = np.dot(Vec_m, np.divide(1+dt*Val_m), Vec_m.T)
-    U_init = np.zeros((N,K))
-    for i in range(N):
-        k = np.random.random_integers(K)
-        U_init[i,k] = 1
+    if N<K:
+        print("Wrong input, N should larger or equal than K.")
     
-    # Ensure each cluster has at least one node
-    
-    # Generate data list that store one node for each cluster
-    K_force = random.sample(range(K),K)
-    
-    # Random select rows with the amount of clusters
-    K_row = random.sample(range(N),K)
-    for j in range(K):
+    else:
+        # Initialize parameters
+        # eigendecomposition of the Laplacian
+        EigVal, EigVec = np.linalg.eig(laplacian_matrix_)
+        X_m = EigVec[0:m,:]
+        Val_m = np.diag(EigVal[0:m])
+        identity_matrix_m = np.diag(np.full(m,1))
+        B_m = (X_m.T).dot(np.linalg.inv(identity_matrix_m+dt/inner_step_count*Val_m)).dot(X_m)
+        U_init = np.zeros((N,K))
+        for i in range(N):
+            k = randrange(K-1)
+            U_init[i,k] = 1
+        
+        # Ensure each cluster has at least one node
+        # Generate data list that store one node for each cluster
+        K_force = random.sample(range(K),K)
+        
+        # Random select rows with the amount of clusters
+        K_row = random.sample(range(N),K)
+        for j in range(K):
 
-        # Force the selected row to be zero vector
-        U_init[K_row[j],:] = np.zeros(K)
+            # Force the selected row to be zero vector
+            U_init[K_row[j],:] = np.zeros(K)
 
-        # Store the data list determined node to this cluster
-        U_init[K_row[j],K_force[j]] = 1
+            # Store the data list determined node to this cluster
+            U_init[K_row[j],K_force[j]] = 1
 
-    # Perform MBO scheme
-    n = 0
-    U_old = U_init.copy()
-    stop_criterion = 0
-    while (stop_criterion > tol | n == 200):  # Diffusion
-        for s in range(inner_step_count):
-            U_half_new = np.matmul(B_m, U_old)
-            U_old = U_half_new
-            s = s + 1 
+        # print(U_init)
+        # Perform MBO scheme
+        n = 0
+        U_old = U_init.copy()
+        stop_criterion = 1
+        while (stop_criterion > tol):  # Diffusion
+            U_half_old = U_old
+            #print(U_old)
+            for s in range(inner_step_count):
+                U_half_new = np.dot(B_m, U_half_old)
+                U_half_old = U_half_new
+                s = s + 1
+            #print(U_half_new)
 
             U_new = np.zeros((N,K))
             for i in range(N):   # Thresholding
-                k = np.max(U_half_new[i,:])
+                k = np.argmax(U_half_new[i,:])
                 U_new[i,k] = 1
-            
+
             # Stop criterion
             Ui_diff = []
             Ui_max = []
             for i in range(N):    
-                Ui_diff.append((np.linalg.norm(U_new[i,:] - U_old[i,:]))^2)
-                Ui_max.append((np.linalg.norm(U_new[i,:]))^2)
+                Ui_diff.append((np.linalg.norm(U_new[i,:] - U_old[i,:]))**2)
+                Ui_max.append((np.linalg.norm(U_new[i,:]))**2)
 
+            #print(U_new)
             max_diff = max(Ui_diff)
             max_new = max(Ui_max)
             stop_criterion = max_diff/max_new
+
+            U_old = U_new
+
+            n = n + 1
+
+            print(stop_criterion,n)
         
-        n = n + 1
+        V_output = []
+        for i in range(N):
+            V_output.append(np.argmax(U_new[i,:]))
+            
+        return V_output
 
-    V_output = []
-    for i in range(N):
-        V_output.append(max(U_new[i,:]))
+    #ARI = adjusted_rand_score(V_output)
+    #print(ARI)
 
-    return V_output
+# test example
+# W is a matrix whose diagonal elements are 1 and the others are -1
+diag_matrix = np.diag(np.full(4,2))
+one_matrix = np.ones((4,4))
+W_matrix = diag_matrix - one_matrix
+# The degree matrix is a diagonal matrix whose entries are the sum of the rows of W_matrix
+degree_matirx = -2*np.diag(np.full(4,1))
+# Compute the Laplacian matrix
+laplacian_matrix = degree_matirx - W_matrix
+
+# Compute V*
+N = laplacian_matrix.shape[0]
+K = laplacian_matrix.shape[0]
+m = K
+V_star = mbo_modularity_eig(N, K, 2, 0.1, laplacian_matrix, 10**(-7), 3)
+print(V_star)
+
+# Calculate ARI
+ground_truth = [0,1,2,3]
+ARI = adjusted_rand_score(V_star, ground_truth)
+print(ARI)
