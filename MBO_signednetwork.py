29d1c95b0aca58e1b3636e707f286f0c40c19915
ZijunLee <65371650+ZijunLee@users.noreply.github.com>
2021-11-25 04:11:52 +0100

    Add files via upload

--- MBO_signednetwork.py
+++ MBO_signednetwork.py
@@ -1,7 +1,22 @@
 import numpy as np
 import random
 from sklearn.metrics.cluster import adjusted_rand_score
+from scipy.linalg import sqrtm
 from random import randrange
+import os
+import sys
+
+from Function_test import A_matrix
+sys.path.append('/'.join(os.getcwd().split('/')[:-1]))
+import scipy.sparse as ss
+import numpy.random as rnd
+import math
+import networkx as nx
+from signet.utils import sqrtinvdiag
+from signet.cluster import Cluster
+from signet.block_models import SSBM
+from sklearn.metrics import adjusted_rand_score
+
 
 """ Parameters
     -----------
@@ -21,7 +36,7 @@ from random import randrange
         signed Laplacian matrix
     ground_truth : ndarray, (n_samples,).{0...K-1} labels.
         labels corresponding to the raw data  
-    """
+"""
 
 def mbo_modularity_eig(N,K,m,dt,laplacian_matrix_, tol ,inner_step_count): # inner stepcount is actually important! and can't be set to 1...
     
@@ -92,7 +107,7 @@ def mbo_modularity_eig(N,K,m,dt,laplacian_matrix_, tol ,inner_step_count): # inn
 
             n = n + 1
 
-            print(stop_criterion,n)
+            #print(stop_criterion,n)
         
         V_output = []
         for i in range(N):
@@ -100,30 +115,79 @@ def mbo_modularity_eig(N,K,m,dt,laplacian_matrix_, tol ,inner_step_count): # inn
             
         return V_output
 
-    #ARI = adjusted_rand_score(V_output)
-    #print(ARI)
+
 
 # test example: signed stochastic block model
-# W is a matrix whose diagonal elements are 1 and the others are -1
-diag_matrix = np.diag(np.full(20,2))
-one_matrix = np.ones((20,20))
-W_matrix = diag_matrix - one_matrix
-#print(W_matrix)
-# The degree matrix is a diagonal matrix whose entries are the sum of the rows of W_matrix
-degree_matirx = np.diag(np.full(20,20))
-#print(degree_matirx)
-# Compute the positive parts of Laplacian matrix
-laplacian_matrix = degree_matirx - W_matrix
-#print(laplacian_matrix)
 
-# Compute V*
-N = laplacian_matrix.shape[0]
-K = laplacian_matrix.shape[0]
+# generate a random graph with community structure by the signed stochastic block model 
+def SSBM(N, K):
+    s_matrix = -np.ones((N,N))
+    randomlist = random.sample(range(1, N), K-1)
+    randomlist.append(0)
+    randomlist.append(N)
+    randomlist.sort()
+    #print(randomlist)
+
+    randomquantity = []
+    for quantity in range(len(randomlist)-1):
+        randomquantity.append(randomlist[quantity+1]-randomlist[quantity])
+    #print(randomquantity)
+
+    for randomnumber in range(len(randomlist)):
+        for i in range(randomlist[randomnumber-1], randomlist[randomnumber]):
+            for j in range(randomlist[randomnumber-1], randomlist[randomnumber]):
+                s_matrix[i][j] = 1
+    #print(s_matrix)
+
+    ground_truth = []
+    for gt in range(len(randomquantity)):
+        ground_truth.extend([gt for y in range(randomquantity[gt])])
+    #print(ground_truth)
+
+    ground_truth_v2 = []
+    for gt in range(len(randomquantity)):
+        ground_truth_v2.extend([randomquantity[gt] for y in range(randomquantity[gt])])
+    #print(ground_truth_v2)
+
+    return s_matrix, ground_truth
+
+def data_generator(s_matrix, noise, sparsity):
+    A_init_matrix = s_matrix
+    N = s_matrix.shape[0]
+    for i in range(N):
+        for j in range(N):
+            if i >= j:
+                A_init_matrix[i][j] = 0
+            if i < j:
+                elements = [A_init_matrix[i][j], 0, -A_init_matrix[i][j]]
+                probabilities = [(1- noise)*sparsity, 1-sparsity, noise*sparsity]
+                A_init_matrix[i][j] = np.random.choice(elements, 1, p=probabilities)
+    A_matrix = A_init_matrix + A_init_matrix.T - np.diag(np.diag(A_init_matrix))
+
+    # calculate the signed laplacian matrix / symmetric normalized laplacian
+    A_absolute_matrix = np.abs(A_matrix)
+    sum_row = np.sum(A_absolute_matrix,axis=1).tolist()
+    deg_diag_m = np.diag(sum_row)
+    deg_inverse_m = sqrtm(deg_diag_m)
+    laplacian_m = deg_diag_m - A_matrix
+    Dbar_half = np.sqrt(deg_inverse_m)
+    laplacian_sym = np.diag(np.full(N,1)) - (Dbar_half).dot(A_matrix).dot(Dbar_half)
+
+    return laplacian_sym
+        
+
+# Parameter setting
+N = 1200
+K = 2
+noise = 0.05
+sparsity = 0.02
 m = K
-V_star = mbo_modularity_eig(N, K, m, 0.1, laplacian_matrix, 10**(-7), 2)
-print(V_star)
+dt = 0.1
+tol = 10**(-7)
+inner_step_count = 3
 
-# Calculate ARI
-ground_truth = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-ARI = adjusted_rand_score(V_star, ground_truth)
+s_matrix, ground_truth = SSBM(N,K)
+laplacian_matrix = data_generator(s_matrix, noise, sparsity)
+V_output = mbo_modularity_eig(N, K, m, dt, laplacian_matrix, tol, inner_step_count)
+ARI = adjusted_rand_score(V_output,ground_truth)
 print(ARI)
