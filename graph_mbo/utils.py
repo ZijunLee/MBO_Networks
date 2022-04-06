4bf423f7e3ac428446efb12f9567ade0424ae754
ZijunLee <65371650+ZijunLee@users.noreply.github.com>
2022-04-06 15:27:41 +0200

    Add files via upload

--- graph_mbo/utils.py
+++ graph_mbo/utils.py
@@ -1,12 +1,12 @@
-import networkx as nx
+#import networkx as nx
 import numpy as np
 import scipy as sp
-import random
-from networkx.algorithms.community import modularity
+#import random
+#from networkx.algorithms.community import modularity
 from scipy.sparse.linalg import eigsh
-from itertools import product
-from random import randrange
-from numpy.random import permutation
+#from itertools import product
+#from random import randrange
+#from numpy.random import permutation
 from sklearn import metrics
 
 
@@ -144,11 +144,11 @@ def apply_threshold(u, target_size, thresh_type):
     return u
 
 
-def get_modularity_original(adj, u):
-    """Calculate the modularity score for the given community structure"""
-    nxgraph = nx.convert_matrix.from_numpy_matrix(adj, create_using=nx.Graph())
-    communities = [np.argwhere(u[:, i]).flatten() for i in range(u.shape[1])]
-    return modularity(nxgraph, communities)
+#def get_modularity_original(adj, u):
+#    """Calculate the modularity score for the given community structure"""
+#    nxgraph = nx.convert_matrix.from_numpy_matrix(adj, create_using=nx.Graph())
+#    communities = [np.argwhere(u[:, i]).flatten() for i in range(u.shape[1])]
+#    return modularity(nxgraph, communities)
 
 
 def spectral_clustering(adj, num_communities):   # adjacancy matrix > 0
@@ -224,7 +224,7 @@ def vector_to_labels(V):
     return np.argmax(V, axis = 1)
 
 
-def get_modularity(network, community_dict):
+#def get_modularity(network, community_dict):
     '''
     Calculate the modularity. Edge weights are ignored.
 
@@ -248,32 +248,32 @@ def get_modularity(network, community_dict):
         The modularity of `network` given `community_dict`
     '''
 
-    Q = 0
-    G = network.copy()
-    nx.set_edge_attributes(G, {e:1 for e in G.edges}, 'weight')
-    A = nx.to_scipy_sparse_matrix(G).astype(float)
+    #Q = 0
+    #G = network.copy()
+    #nx.set_edge_attributes(G, {e:1 for e in G.edges}, 'weight')
+    #A = nx.to_scipy_sparse_matrix(G).astype(float)
 
-    if type(G) == nx.Graph:
+    #if type(G) == nx.Graph:
         # for undirected graphs, in and out treated as the same thing
-        out_degree = in_degree = dict(nx.degree(G))
-        M = 2.*(G.number_of_edges())
+    #    out_degree = in_degree = dict(nx.degree(G))
+    #    M = 2.*(G.number_of_edges())
         #print("Calculating modularity: ")
     #elif type(G) == nx.DiGraph:
     #    in_degree = dict(G.in_degree())
     #    out_degree = dict(G.out_degree())
     #    M = 1.*G.number_of_edges()
     #    print("Calculating modularity for directed graph")
-    else:
-        print('Invalid graph type')
-        raise TypeError
+    #else:
+    #    print('Invalid graph type')
+    #    raise TypeError
 
-    nodes = list(G)
-    Q = np.sum([A[i,j] - in_degree[nodes[i]]*\
-                         out_degree[nodes[j]]/M\
-                 for i, j in product(range(len(nodes)),\
-                                     range(len(nodes))) \
-                if community_dict[nodes[i]] == community_dict[nodes[j]]])
-    return Q / M
+    #nodes = list(G)
+    #Q = np.sum([A[i,j] - in_degree[nodes[i]]*\
+    #                     out_degree[nodes[j]]/M\
+    #             for i, j in product(range(len(nodes)),\
+    #                                 range(len(nodes))) \
+    #            if community_dict[nodes[i]] == community_dict[nodes[j]]])
+    #return Q / M
 
 
 
