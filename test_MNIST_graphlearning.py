b6eed07cc09bc5eb78051e189cf1243176d492aa
ZijunLee <65371650+ZijunLee@users.noreply.github.com>
2022-04-10 04:36:34 +0200

    Add files via upload

--- test_MNIST_graphlearning.py
+++ test_MNIST_graphlearning.py
@@ -36,14 +36,15 @@ gamma_1 =1
 
 data, gt_labels = gl.datasets.load('mnist')
 #gt_list = gt_labels.tolist()
-print(data.shape)
-print(type(data))
+#print(data.shape)
+#print(type(data))
 print('gt shape: ', gt_labels.shape)
 
 
 #pca = PCA(n_components = 50)
 #train_data = pca.fit_transform(data)
 #train_data = pca.transform(sample_data)
+#print('train_data shape: ', type(train_data))
 
 #del data
 
@@ -54,14 +55,15 @@ gamma = 0.02
 
 feature_map_nystroem = Nystroem(gamma=gamma,random_state=1,n_components=50)
 Z_training = feature_map_nystroem.fit_transform(data)
-print('Z_training shape: ', Z_training.shape)
+#print('Z_training: ', Z_training)
+#print('Z_training shape: ', Z_training.shape)
 
-n1, p = Z_training.shape
-print("Features:", p)
+#n1, p = Z_training.shape
+#print("Features:", p)
 
-#W = gl.weightmatrix.knn(data, 10)
-#print('W shape: ', W.shape)
-#print('W type: ', type(W))
+W = gl.weightmatrix.knn(Z_training, 10)
+print('W shape: ', W.shape)
+print('W type: ', type(W))
 
 
 #gt_labels = gl.datasets.load('mnist', labels_only=True)
@@ -80,12 +82,16 @@ print("Features:", p)
 
 #del train_data
 
-adj_mat = build_affinity_matrix_new(Z_training,gamma=gamma, affinity='rbf',n_neighbors=10, neighbor_type='knearest')
-print('adj_mat shape: ', adj_mat.shape)
+#adj_mat = build_affinity_matrix_new(Z_training,gamma=gamma, affinity='rbf',n_neighbors=10, neighbor_type='knearest')
+#print('adj_mat shape: ', adj_mat.shape)
+adj_mat = W.toarray()
+#print('adj_mat type: ', type(adj_mat))
 
 del Z_training
 
 null_model = construct_null_model(adj_mat)
+print('null_model shape: ', null_model.shape)
+
 
 num_nodes, m_1, degree, target_size, graph_laplacian, sym_graph_lap,rw_graph_lap, signless_laplacian, sym_signless_lap, rw_signless_lap = adj_to_laplacian_signless_laplacian(adj_mat, null_model, num_communities,m ,target_size=None)
 
@@ -136,7 +142,7 @@ print('HU original MBO the num_iteration: ', num_iter_HU)
 
 u_hu_label_1 = vector_to_labels(u_hu_vector)
 
-modu_hu_original_1 = skn.clustering.modularity(adj_mat,u_hu_label_1,resolution=0.5)
+modu_hu_original_1 = skn.clustering.modularity(W,u_hu_label_1,resolution=0.5)
 ARI_hu_original_1 = adjusted_rand_score(u_hu_label_1, gt_labels)
 purify_hu_original_1 = purity_score(gt_labels, u_hu_label_1)
 inverse_purify_hu_original_1 = inverse_purity_score(gt_labels, u_hu_label_1)
@@ -160,7 +166,7 @@ print('u_1 nor L_F & Q_H number of iteration(K=10 and m=K): ', num_repeat_1_nor_
 u_1_nor_Lf_Qh_individual_label_1 = vector_to_labels(u_1_nor_Lf_Qh_individual_1)
 
 
-modularity_1_nor_lf_qh = skn.clustering.modularity(adj_mat,u_1_nor_Lf_Qh_individual_label_1,resolution=0.5)
+modularity_1_nor_lf_qh = skn.clustering.modularity(W,u_1_nor_Lf_Qh_individual_label_1,resolution=0.5)
 ARI_mbo_1_nor_Lf_Qh_1 = adjusted_rand_score(u_1_nor_Lf_Qh_individual_label_1, gt_labels)
 purify_mbo_1_nor_Lf_Qh_1 = purity_score(gt_labels, u_1_nor_Lf_Qh_individual_label_1)
 inverse_purify_mbo_1_nor_Lf_Qh_1 = inverse_purity_score(gt_labels, u_1_nor_Lf_Qh_individual_label_1)
@@ -184,7 +190,7 @@ print('MMBO1 with inner step & sym the num_repeat_inner_nor: ',num_repeat_inner_
 
 u_inner_nor_label_1 = vector_to_labels(u_inner_nor_1)
 
-modularity_1_inner_nor_1 = skn.clustering.modularity(adj_mat,u_inner_nor_label_1,resolution=0.5)
+modularity_1_inner_nor_1 = skn.clustering.modularity(W,u_inner_nor_label_1,resolution=0.5)
 ARI_mbo_1_inner_nor_1 = adjusted_rand_score(u_inner_nor_label_1, gt_labels)
 purify_mbo_1_inner_nor_1 = purity_score(gt_labels, u_inner_nor_label_1)
 inverse_purify_mbo_1_inner_nor_1 = inverse_purity_score(gt_labels, u_inner_nor_label_1)
@@ -206,7 +212,7 @@ louvain_list = list(dict.values(partition_Louvain))    #convert a dict to list
 louvain_array = np.asarray(louvain_list)
 
 
-modularity_louvain = skn.clustering.modularity(adj_mat,louvain_array,resolution=1)
+modularity_louvain = skn.clustering.modularity(W,louvain_array,resolution=1)
 ARI_louvain = adjusted_rand_score(louvain_array, gt_labels)
 purify_louvain = purity_score(gt_labels, louvain_array)
 inverse_purify_louvain = inverse_purity_score(gt_labels, louvain_array)
@@ -230,7 +236,7 @@ ass_vec = labels_to_vector(assignment)
 ass_dict = label_to_dict (assignment)
 
 
-modularity_spectral_clustering = skn.clustering.modularity(adj_mat,assignment,resolution=0.5)
+modularity_spectral_clustering = skn.clustering.modularity(W,assignment,resolution=0.5)
 ARI_spectral_clustering = adjusted_rand_score(assignment, gt_labels)
 purify_spectral_clustering = purity_score(gt_labels, assignment)
 inverse_purify_spectral_clustering = inverse_purity_score(gt_labels, assignment)
